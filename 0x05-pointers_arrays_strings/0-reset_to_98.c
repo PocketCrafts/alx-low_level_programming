b375d1c27d17e10002a95d451b5396c0185e5d3a
Nadu C <charawe.yw@gmail.com>
2025-03-24 11:14:51 +0300

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
-void reset_to_98(int *n){
+/**
+ * reset_to_98 - function
+ *
+ * Description: resets value of an int to 98
+ * @n: pointer to an int
+ */
+
+void reset_to_98(int *n)
+{
 	*n = 98;
 }
