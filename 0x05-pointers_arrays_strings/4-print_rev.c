8f6fb4472fa3d53c970751ffa9417ac2cf2c0558
Nadu C <charawe.yw@gmail.com>
2025-03-24 14:42:08 +0300

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ *
+ * Description: prints a string in reverse
+ * @s: pointer to a char array
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) - 1; *(s + i) >= 0 ; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
