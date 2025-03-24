d2feffa6b66a45ca1e6c723aa2d525887766f71d
Nadu C <charawe.yw@gmail.com>
2025-03-24 14:28:41 +0300

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ *
+ * Description: computes length of string
+ * @s: pointer to a char array
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i + 1);
+}
