656f3f1329585742eedb3410801c2368fa586a7d
Nadu C <charawe.yw@gmail.com>
2025-03-31 22:00:56 +0300

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - function
+ *
+ * Description: capitalizes words in a string
+ * @s: pointer to string
+ * Return: pointer to capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int x;
+	int y;
+	char *t = " \t\n,;.!?\"(){}\0";
+
+	for (x = 0; t[x] != '\0'; x++)
+	{
+		for (y = 0; s[y] != '\0'; y++)
+		{
+			if (s[y] == t[x])
+			{
+				if (s[y + 1] > 96 && s[y + 1] < 123)
+					s[y + 1] -= 32, y++;
+			}
+		}
+	}
+	return (s);
+}
