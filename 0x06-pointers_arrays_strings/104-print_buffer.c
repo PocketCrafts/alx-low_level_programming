af2897a7c62c2c3860feaed03fc4fd20d66a86bd
Nadu C <charawe.yw@gmail.com>
2025-04-28 19:28:53 +0300

    12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,52 +1,5 @@
 #include "main.h"
-
-/**
- * to_hex - function
- *
- * Description: converts decimal to hex
- * @dec: decimal number
- * @ptr: pointer to store hex string
- * Return: pointer to 2-digit hex string
- */
-
-char *to_hex(char *ptr, int dec)
-{
-	int x = 7;
-
-	for (; x > 0; x--)
-	{
-		if (dec > 0)
-		{
-			ptr[x] = (dec % 16) < 10 ? (dec % 16) + 48 :
-				(dec % 16) - 10 + 97;
-			dec /= 16;
-		}
-		else
-			ptr[x] = 48;
-	}
-	return (ptr);
-}
-
-/**
- * print_chars - function
- *
- * Description: prints a string given size
- * @str: pointer to string
- * @size: size of bytes to print
- */
-
-void print_chars(char *str, int size)
-{
-	int x;
-
-	for (x = 0; x < size; x++)
-	{
-		if (str[x] > 31 && str[x] < 127)
-			_putchar(str[x]);
-		else
-			_putchar(46);
-	}
-}
+#include <stdio.h>
 
 /**
  * print_buffer - function
@@ -59,22 +12,22 @@ void print_chars(char *str, int size)
 void print_buffer(char *b, int size)
 {
 	int x, y;
-	char ptr[9] = "00000000";
 
 	for (x = 0; x < size; x += 10)
 	{
-		print_chars(to_hex(ptr, x), 8), _putchar(':');
-		for (y = x; y < x + 10; y++)
+		if (x % 10 == 0)
+			printf("%08x: ", x), y = x;
+		for (y = x; y < x + 10; y += 2)
+			y < size? printf("%02x%02x ", b[y], b[y + 1]) :
+				printf("%5c", 32);
+		for (y = x; y < size && y < x + 10; y++)
 		{
-			if (y % 2 == 0)
-				_putchar(' ');
-			if (y < size)
-				print_chars(&to_hex(ptr, b[y])[6], 2);
+			if (b[y] > 31 && b[y] < 127)
+				printf("%c", b[y]);
 			else
-				_putchar(' '), _putchar(' ');
+				printf(".");
 		}
-		_putchar(' ');
-		print_chars(&b[x], x + 10 > size ? size - x : 10), _putchar('\n');
+		printf("\n");
 	}
-	_putchar('\n');
+	printf("\n");
 }
